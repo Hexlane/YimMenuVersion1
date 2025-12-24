8736af6b63329507d446d9f5aad0488413dd8730
Hexlane <deede3771@gmail.com>
2025-12-23 23:42:19 -0500

    New socialclub/sc_news.hpp

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -1,15 +1,8 @@
 include(FetchContent)
-
+message(STATUS "Fetching GTAV-Classes (sc-news)")
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/Mr-X-GTA/GTAV-Classes-1.git
-    GIT_TAG        b74e688860b0df47d7ec87c1eb6227ee853fc90a
-    GIT_PROGRESS TRUE
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
+    GIT_REPOSITORY https://github.com/Hexlane/GTAV-Classes-1.git
+    GIT_TAG sc-news
 )
-message("GTAV-Classes")
-if(NOT gtav_classes_POPULATED)
-    FetchContent_Populate(gtav_classes)
-endif()
-
+FetchContent_MakeAvailable(gtav_classes)
